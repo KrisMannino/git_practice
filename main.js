7e917dec7aa3b43b6e82ef81f1618aa5d2f90908
Kris Mannino <krismannino@gmail.com>
2022-03-06 15:21:08 -0500

    Revert "Merge pull request #3 from KrisMannino/javascript"
    This reverts commit ca21a189cf7ab7f47305e7004d472ea3602a74d2, reversing
    changes made to 2068d4b397bb72057c41d2876cc9682d16986797.

--- main.js
+++
@@ -1,8 +0,0 @@
-function toggleImage() {
-    var img1 = "http://placeimg.com/640/480/animals";
-    var img2 = "http://placeimg.com/640/480/arch";
-    
-    var imgElement = document.getElementById('toggleImage');
- 
-    imgElement.src = (imgElement.src === img1)? img2 : img1;
- }
